e44ae98d99aaf3ba837d72a9b998a4cda0efc5bc
Kristofer <kroldan@ford.com>
2022-04-20 13:27:30 -0400

    Add grid images

--- site/index.js
+++ site/index.js
@@ -1,13 +1,13 @@
 const apiUrl = "https://api.thedogapi.com/v1/breeds";
 const dogApiKey = "56ca9d3f-7947-4e93-893b-beea68ab3430";
 const spinner = document.querySelector(".spinner");
+const header = document.querySelector("header");
 
 function getRandomElement(array){
     return array[(Math.random() * array.length) | 0]
 }
 
-function getRandomImage(array){
-    const header = document.querySelector("header");
+function getRandomHeaderImage(array){
     let randomBreed = getRandomElement(array);
     let breedImage = document.createElement("figure");
     breedImage.innerHTML = `
@@ -16,12 +16,23 @@ function getRandomImage(array){
     header.append(breedImage)
 }
 
-/*const testArray = ["Poodle", "Golden Doodle", "Pitbull", "Golden Retriever", "Dalmatian", "Beagle", "Yorkee"]
-console.log(getRandomElement(testArray));*/
+function createRandomGridImage(array){
+    const breedGrid = document.querySelector(".grid-images");
+    const randomBreedArray = _.sampleSize(array, 6);
+    randomBreedArray.forEach(element => {    
+        let singleGridImage = document.createElement("figure");
+        singleGridImage.innerHTML = `
+            <img src="${element.image.url}" alt="${element.name}"/>
+            <figcaption>${element.name}</figcaption>
+        `
+        breedGrid.append(singleGridImage)
+    })
+}
 
 fetch(apiUrl).then(response => response.json())
     .then(parsedResponse => {
         console.log(parsedResponse);
-        getRandomImage(parsedResponse);
+        getRandomHeaderImage(parsedResponse);
+        createRandomGridImage(parsedResponse);
         spinner.classList.add("hidden");
     });
